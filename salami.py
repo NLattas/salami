6e061a91ac83b08d82644b3bdecf878187563b16
Nikolas <nikolax2004@gmail.com>
2018-12-23 21:30:50 +0200

    imports text

--- salami.py
+++ salami.py
@@ -3,9 +3,18 @@
 #
 
 # Import from text file
+def import_text():
+    '''
+    opens txt file with one row to be translated
+    '''
+    text_file = open('text.txt','r') 
+    text = text_file.read()
+    return text
 
 # Import Letters
 
 # Generate new text
 
 # Output
+
+print(import_text())
